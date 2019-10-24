34811ed6c1a0186e4778e01841ba864215504308
Elisha Johnson <elishamagicjohnson@gmail.com>
2019-10-24 18:30:59 -0500

    events practice

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -12,4 +12,11 @@ export class MovieListComponent implements OnInit {
 
    ngOnInit() {
    }
+
+   addMovie(newTitle: string) {
+      newTitle = newTitle.trim();
+      if (newTitle !== "" && !this.movies.includes(newTitle)) {
+         this.movies.push(newTitle);
+      }
+   }
 }
