86081d5b2759277ee9c0f2192482d40c63a47ebd
Elisha Johnson <elishamagicjohnson@gmail.com>
2019-10-25 18:46:00 -0500

    part 3 examples

--- lesson3/examples/src/app/buttons/buttons.component.ts
+++ lesson3/examples/src/app/buttons/buttons.component.ts
@@ -7,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ButtonsComponent implements OnInit {
    buttonHeading: string = "Buttons"
-   inactive: boolean = false;
+   goldInactive: boolean = false;
+   silverInactive: boolean = false;
+   copperInactive: boolean = false;
+   trickInactive: boolean = false;
+   currentPosition: number = 1;
+   newPosition: number = 1;
 
    constructor() { }
-
+   s
    ngOnInit() { }
 
+   reactivate() {
+      this.goldInactive = false;
+      this.silverInactive = false;
+      this.copperInactive = false;
+   }
+
+   moveButton() {
+      while (this.newPosition === this.currentPosition) {
+         this.newPosition = Math.floor(Math.random() * 3);
+      }
+      this.currentPosition = this.newPosition;
+      if (this.newPosition === 0) {
+         document.getElementById("moving-button").style.removeProperty("right")
+         document.getElementById("moving-button").style.position = "absolute";
+         document.getElementById("moving-button").style.left = "0";
+      } else if (this.newPosition === 1) {
+         document.getElementById("moving-button").style.removeProperty("right")
+         document.getElementById("moving-button").style.removeProperty("left")
+         document.getElementById("moving-button").style.removeProperty("position")
+      } else {
+         document.getElementById("moving-button").style.removeProperty("left")
+         document.getElementById("moving-button").style.position = "absolute";
+         document.getElementById("moving-button").style.right = "0";
+      }
+   }
 }
